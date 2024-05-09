9265131c3393a4e12d8c2f54b6a961a72a5f41c1
AdrianCraft <proadrian684@gmail.com>
2024-05-09 01:20:10 -0600

    multiple validators

--- src/_tokenize.rs
+++ src/_tokenize.rs
@@ -2,8 +2,17 @@ pub struct Position {
     pub line: usize,
     pub column: usize,
 }
+impl Clone for Position {
+    fn clone(&self) -> Self {
+        Position {
+            line: self.line,
+            column: self.column,
+        }
+    }
+}
+impl Copy for Position {}
 
-pub struct Token<TokenKind> {
+pub struct Token< TokenKind> {
     pub token_type: TokenKind,
     pub value: String,
     pub position: Position,
@@ -11,26 +20,40 @@ pub struct Token<TokenKind> {
 
 pub type TokenOptionsCallback<TK> = fn(char, Position, String) -> (Token<TK>, usize);
 
-pub type TokenOptions<'a, TK> = Vec<(&'a str, TokenOptionsCallback<TK>)>;
+pub enum TokenOptionCondition {
+    Chars(&'static str),
+    Fn(fn(char) -> bool),
+}
 
-pub fn tokenize<TK>(input: String, options: TokenOptions<TK>) -> Result<Vec<Token<TK>>, Box<dyn std::error::Error>> {
+pub type TokenOption<'a, TK> = (TokenOptionCondition, TokenOptionsCallback<TK>);
+
+pub fn tokenize<TK>(
+    input: String,
+    options: Vec<TokenOption<TK>>,
+) -> Result<Vec<Token<TK>>, Box<dyn std::error::Error>> {
     let lines = input.lines();
     let mut tokens = Vec::new();
     for (line_number, line) in lines.enumerate() {
         let mut column = 0;
         for c in line.chars() {
             let mut token: Option<Token<TK>> = None;
-            for (ch, option) in &options {
-                let chars = *ch;
-                if crate::_is_valid_char::is_valid_char(chars, c) == false {
+            for (condition, result) in &options {
+                let is_valid = match condition {
+                    TokenOptionCondition::Chars(chars) => chars.contains(c),
+                    TokenOptionCondition::Fn(f) => f(c),
+                };
+                if !is_valid {
                     continue;
                 }
-                let (t, consumed) = option(c, Position { line: line_number, column }, line.to_string());
+                let position = Position {
+                    line: line_number,
+                    column,
+                };
+                let (t, consumed) = result(c, position, line.to_string());
                 token = Some(t);
                 column += consumed;
                 break;
             }
-
             if let Some(token) = token {
                 tokens.push(token);
             } else {
