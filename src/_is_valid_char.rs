d8403f5360f1ed795438663bc39c1433407bd824
AdrianCraft <proadrian684@gmail.com>
2024-10-20 23:32:35 -0600

    location

--- src/_is_valid_char.rs
+++
@@ -1,8 +0,0 @@
-pub fn is_valid_char(valid_chars: &str, eval_char: char) -> bool {
-    for c in valid_chars.chars() {
-        if c == eval_char {
-            return true;
-        }
-    }
-    false
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
-mod _is_valid_char;
 mod _tokenize;
 pub mod list;
 
-pub use _tokenize::{*};
-pub use _is_valid_char::{*};
-pub use list::{*};
+pub use _tokenize::*;
+pub use list::*;
+pub fn is_valid_char(valid_chars: &str, eval_char: char) -> bool {
+    for c in valid_chars.chars() {
+        if c == eval_char {
+            return true;
+        }
+    }
+    false
+}
+pub fn get_content(text: &str, start: Position, end: Position) -> Option<String> {
+    if start >= end {
+        return None;
+    }
+    let mut string = String::new();
+    let lines = text.lines();
+    for (line_number, line) in lines.enumerate() {
+      if start.line < line_number {continue;}
+      if end.line > line_number {break;}
+      for (char_number, char) in line.char_indices(){
+        if start.column < char_number{continue;}
+        if end.line == line_number && end.column > char_number {break;}
+        string.push(char);
+      }
+    }
+    Some(string)
+}
